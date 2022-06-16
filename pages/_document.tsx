2324013243fb40f9b4603ac580698f43d5548875
biglol <biglol@nate.com>
2022-06-17 00:34:22 +0900

    testing ico file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,6 +13,7 @@ export default function Document() {
 					href="https://fonts.googleapis.com/icon?family=Material+Icons"
 					rel="stylesheet"
 				/>
+				<link rel="shortcut icon" href="/favicon.ico" />
 			</Head>
 			<body>
 				<Main />
