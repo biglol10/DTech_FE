ea107b60c1fbaa4bedcf2f82051498a3c40763cb
biglol <biglol@nate.com>
2022-06-26 16:19:30 +0900

    tab component done

--- components/index.tsx
+++ components/index.tsx
@@ -5,5 +5,27 @@ import InputSearch from './input/InputSearch';
 import InputWithIcon from './input/InputWithIcon';
 import Button from './button/Button';
 import Label from './label/Label';
+import Accordion from './accordion/Accordion';
+import CheckboxListDefault from './checkbox/CheckboxListDefault';
+import List from './list/List';
+import Box from './box/Box';
+import { Tabs, TabHeader, Tab, TabPanels, TabPanel } from './tab/Tab';
 
-export { Button, InputDefault, InputSearch, InputWithIcon, Label, ModalPopup, CheckboxDefault };
+export {
+	Accordion,
+	Box,
+	Button,
+	InputDefault,
+	InputSearch,
+	InputWithIcon,
+	Label,
+	List,
+	ModalPopup,
+	CheckboxDefault,
+	CheckboxListDefault,
+	Tabs,
+	TabHeader,
+	Tab,
+	TabPanels,
+	TabPanel,
+};
