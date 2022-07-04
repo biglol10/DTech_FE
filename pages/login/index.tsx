9ed9a6494e56e079a246369b1b2e6733106a5a31
변지욱 <sgo1017099@mobmail.shinhan.com>
2022-07-04 17:29:50 +0900

    feature/JW/loginpage 3 login images done

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -3,27 +3,56 @@ import { useState } from 'react';
 import { Icon } from 'semantic-ui-react';
 import LeftBackground1 from '@public/background/loginLeft.png';
 import LeftBackground2 from '@public/background/loginLeft2.png';
+import LeftBackground3 from '@public/background/loginLeft3.png';
+import DLogo from '@public/images/DLogo2.png';
+import classNames from 'classnames/bind';
 import Style from './Login.module.scss';
 
 const Login = () => {
-	const leftBackground = [LeftBackground1, LeftBackground2];
+	const cx = classNames.bind(Style);
+	const leftBackground = [LeftBackground1, LeftBackground2, LeftBackground3];
+	const iconColorList = ['white', 'black', 'cadetblue'];
 
 	const [leftBackgroundIndex, setLeftBackgroundIndex] = useState(0);
 
 	return (
-		<div className={Style['loginDiv']}>
-			<main className={Style['loginMain']}>
+		<div className={cx('loginDiv')}>
+			<main className={cx('loginMain')}>
 				<div
-					className={Style['loginMainLeft']}
+					className={cx('loginMainLeft')}
 					style={{ backgroundImage: `url(${leftBackground[leftBackgroundIndex].src})` }}
 				>
-					<aside className={Style['loginBottomAside']}>
+					{leftBackgroundIndex === 0 && (
+						<article className={cx('loginCenterArticle_0')}>
+							<p>
+								<Image src={DLogo} width={48} height={48} /> Dtech App
+							</p>
+							<p>Explore your team&apos;s</p>
+							<p>skill-set & share info</p>
+						</article>
+					)}
+
+					{leftBackgroundIndex === 1 && (
+						<article className={cx('loginCenterArticle_1')}>
+							<p>Whom to ask?</p>
+							<p>Who has what skill?</p>
+							<p>Find it here!</p>
+						</article>
+					)}
+
+					{leftBackgroundIndex === 2 && (
+						<article className={cx('loginCenterArticle_2')}>
+							<p>Explore the app now!</p>
+						</article>
+					)}
+
+					<aside className={cx('loginBottomAside')}>
 						<Icon
 							name="arrow alternate circle left outline"
 							size="huge"
-							className={Style['loginBottomAside_icon']}
+							className={cx('loginBottomAside_icon')}
 							style={{
-								'--iconColor': `${leftBackgroundIndex === 1 ? 'black' : 'white'}`,
+								'--iconColor': `${iconColorList[leftBackgroundIndex]}`,
 							}}
 							onClick={() =>
 								setLeftBackgroundIndex((prev) => {
@@ -38,7 +67,7 @@ const Login = () => {
 						<Icon
 							name="arrow alternate circle right outline"
 							size="huge"
-							className={Style['loginBottomAside_icon']}
+							className={cx('loginBottomAside_icon')}
 							onClick={() =>
 								setLeftBackgroundIndex((prev) => {
 									if (prev + 1 > leftBackground.length - 1) {
@@ -49,13 +78,12 @@ const Login = () => {
 								})
 							}
 							style={{
-								'--iconColor': `${leftBackgroundIndex === 1 ? 'black' : 'white'}`,
+								'--iconColor': `${iconColorList[leftBackgroundIndex]}`,
 							}}
 						/>
 					</aside>
-					{/* <Image src={leftPic} className={Style['loginMainLeftImage']}/> */}
 				</div>
-				<div style={{ backgroundColor: 'white', width: '35%', height: '80%' }}>
+				<div style={{ backgroundColor: 'white', width: '25%', height: '80%' }}>
 					asfd2222
 				</div>
 			</main>
