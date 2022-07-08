0eaa895a001ea4e603e6a4e34e89e456c42232d8
변지욱 <sgo1017099@mobmail.shinhan.com>
2022-07-08 17:57:20 +0900

    feature/JW/loginRequest loginRequest on FE with redux done

--- saga/index.ts
+++ saga/index.ts
@@ -3,7 +3,8 @@ import { all, fork } from 'redux-saga/effects';
 import counterSaga from './sagaCounter';
 import testApiSaga from './testApiSaga';
 import modalSaga from './modalSaga';
+import authSaga from './authSaga';
 
 export default function* rootSaga() {
-	yield all([fork(counterSaga), fork(testApiSaga), fork(modalSaga)]);
+	yield all([fork(counterSaga), fork(testApiSaga), fork(modalSaga), fork(authSaga)]);
 }
