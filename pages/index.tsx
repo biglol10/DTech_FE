618620e3d547cfaa589eab1724d78304b2a945d3
변지욱 <sgo1017099@mobmail.shinhan.com>
2022-07-11 18:02:56 +0900

    doing layout2

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,25 @@ const Index = () => {
 				style={{ width: '100%', height: '100vh', display: 'flex', backgroundColor: 'red' }}
 			>
 				<div className="left" style={{ height: '100%', display: 'flex' }}>
-					<div className="sidebar" style={{ width: '80px', height: '100%' }}></div>
+					<div
+						className="sidebar"
+						style={{
+							width: '80px',
+							height: '100%',
+							backgroundImage:
+								'linear-gradient(-225deg, #5271c4 0%, #b19fff 48%, #eca1fe 100%)',
+							display: 'flex',
+						}}
+					>
+						<div className="items" style={{ display: 'flex', flexDirection: 'column' }}>
+							<img src="https://i.ibb.co/L8D5T60/light.png" />
+							<img src="https://i.ibb.co/zmDbMVZ/diamond.png" />
+							<img src="https://i.ibb.co/W5QZ9Fk/envelope.png" />
+							<img src="https://i.ibb.co/CnKDBxC/flask.png" />
+							<img src="https://i.ibb.co/MGs4Fyn/sent-mail.png" />
+							<img src="https://i.ibb.co/zGtDpcp/map.png" />
+						</div>
+					</div>
 				</div>
 			</div>
 		</>
