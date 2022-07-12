7a8a20f7b2c3a786cd664a9b16e11a3b88301911
변지욱 <sgo1017099@mobmail.shinhan.com>
2022-07-12 13:51:43 +0900

    sidebar and chatlayout almost done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import Image from 'next/image';
 import DLogo from '@public/images/DLogo2.png';
 import { InputLayout, InputDefault } from '@components/index';
@@ -11,7 +11,7 @@ const Index = () => {
 		<>
 			<div className={Style['mainLayout']}>
 				<div className={Style['left']}>
-					<div className={Style['sidebar']}>
+					<nav className={Style['sidebar']}>
 						<div className={Style['wrapper']}>
 							<img src="https://i.ibb.co/L8D5T60/light.png" />
 							<img src="https://i.ibb.co/zmDbMVZ/diamond.png" />
@@ -20,39 +20,89 @@ const Index = () => {
 							<img src="https://i.ibb.co/MGs4Fyn/sent-mail.png" />
 							<img src="https://i.ibb.co/zGtDpcp/map.png" />
 						</div>
-					</div>
+					</nav>
 					<div className={Style['sidebarChat']}>
 						<div className={Style['chatLogo']}>
 							<Image src={DLogo} width={48} height={48} /> Dtech App
 						</div>
-						<div className={Style['userSearch']}>
-							<InputLayout
-								stretch={true}
-								inputLabel="사용자 검색"
-								inputLabelSize="h5"
-								showInputLabel={true}
-								spacing={32}
-							>
-								<InputDefault
-									id="userSearchInput"
-									placeholder="사용자 검색"
-									value={userSearch}
-									size="small"
-									onChange={(obj: { value: string }) => {
-										setUserSearch(obj.value);
-									}}
-									className="userSearchInput"
-								/>
-							</InputLayout>
-						</div>
 
-						<div className={Style['divider']}>
-							<span></span>
-							<span>Online</span>
-							<span></span>
+						<div className={Style['chatArea']}>
+							<div className={Style['userSearch']}>
+								<InputLayout
+									stretch={true}
+									inputLabel="사용자 검색"
+									inputLabelSize="h5"
+									showInputLabel={true}
+									spacing={32}
+								>
+									<InputDefault
+										id="userSearchInput"
+										placeholder="사용자 검색"
+										value={userSearch}
+										size="small"
+										onChange={(obj: { value: string }) => {
+											setUserSearch(obj.value);
+										}}
+										className="userSearchInput"
+									/>
+								</InputLayout>
+							</div>
+
+							<div className={Style['usersInfo']}>
+								<div className={Style['divider']}>
+									<span></span>
+									<span>온라인</span>
+									<span></span>
+								</div>
+
+								<div className={Style['usersOnline']}>
+									{Array(3)
+										.fill(0)
+										.map((item, idx) => (
+											<div
+												className={Style['folder-icons']}
+												key={`online_${idx}`}
+											>
+												<div className={Style['user-avatar']}>
+													<div className={Style['user-online']}></div>
+													<img src="https://randomuser.me/api/portraits/women/71.jpg" />
+												</div>
+												<div className={Style['username']}>
+													User Online{idx}
+												</div>
+											</div>
+										))}
+								</div>
+
+								<div className={Style['divider']}>
+									<span></span>
+									<span>오프라인</span>
+									<span></span>
+								</div>
+
+								<div className={Style['usersOffline']}>
+									{Array(20)
+										.fill(0)
+										.map((item, idx) => (
+											<div
+												className={Style['folder-icons']}
+												key={`offline_${idx}`}
+											>
+												<div className={Style['user-avatar']}>
+													<div className={Style['user-offline']}></div>
+													<img src="https://randomuser.me/api/portraits/women/71.jpg" />
+												</div>
+												<div className={Style['username']}>
+													User Offline{idx}
+												</div>
+											</div>
+										))}
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
+				<div className={Style['right']}></div>
 			</div>
 		</>
 	);
