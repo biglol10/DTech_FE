ecb574ad1dd59e3f14236285ff4c0a86efe3b23d
변지욱 <sgo1017099@mobmail.shinhan.com>
2022-07-15 17:32:17 +0900

    quill testing

--- pages/examplePage/index.tsx
+++ pages/examplePage/index.tsx
@@ -1,4 +1,5 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useMemo } from 'react';
+import dynamic from 'next/dynamic';
 import Testcomponent from '@components/Testcomponent';
 import { useSelector, useDispatch } from 'react-redux';
 import { increment } from '@store/counterSlice';
@@ -44,6 +45,93 @@ const options = [
 	{ key: 'ui', text: 'UI Design', value: 'ui' },
 	{ key: 'ux', text: 'User Experience', value: 'ux' },
 ];
+
+const formats = [
+	'header',
+	'font',
+	'size',
+	'bold',
+	'italic',
+	'underline',
+	'strike',
+	'blockquote',
+	'list',
+	'bullet',
+	'indent',
+	'link',
+	'image',
+	'video',
+];
+
+const ReactQuill = dynamic(
+	async () => {
+		const { default: RQ } = await import('react-quill');
+
+		return function comp({ forwardedRef, ...props }: any) {
+			return <RQ ref={forwardedRef} {...props} />;
+		};
+	},
+	{ ssr: false },
+);
+
+const ReactQuillContainer = ({ description, setDescription }: any) => {
+	const quillRef = useRef();
+
+	const imageHandler = () => {
+		const input = document.createElement('input');
+
+		input.setAttribute('type', 'file');
+		input.setAttribute('accept', 'image/*');
+		document.body.appendChild(input);
+
+		input.click();
+
+		input.onchange = async () => {
+			// const [file] = input.files;
+			console.log(input.files);
+
+			// // S3 Presigned URL로 업로드하고 image url 받아오기
+			// const { preSignedPutUrl: presignedURL, readObjectUrl: imageURL } = (
+			// 	await getS3PresignedURL(file.name)
+			// ).data;
+			// await uploadImage(presignedURL, file);
+
+			// // 현재 커서 위치에 이미지를 삽입하고 커서 위치를 +1 하기
+			// const range = quillRef.current.getEditorSelection();
+			// quillRef.current.getEditor().insertEmbed(range.index, 'image', imageURL);
+			// quillRef.current.getEditor().setSelection(range.index + 1);
+			// document.body.querySelector(':scope > input').remove();
+		};
+	};
+
+	// useMemo를 사용한 이유는 modules가 렌더링마다 변하면 에디터에서 입력이 끊기는 버그가 발생
+	const modules = useMemo(
+		() => ({
+			toolbar: {
+				container: [
+					[{ header: [1, 2, false] }],
+					['bold', 'italic', 'underline', 'strike', 'blockquote'],
+					[{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
+					['link', 'image'],
+					['clean'],
+				],
+				handlers: { image: imageHandler },
+			},
+		}),
+		[],
+	);
+
+	return (
+		<ReactQuill
+			forwardedRef={quillRef}
+			placeholder="본문을 입력하세요..."
+			modules={modules}
+			formats={formats}
+			value={description}
+			onChange={setDescription}
+		/>
+	);
+};
 
 const Index = () => {
 	const [inputValue, setInputValue] = useState(2);
@@ -230,6 +318,11 @@ const Index = () => {
 					options={options}
 				/>
 			</InputLayout>
+
+			<br />
+			<br />
+
+			<ReactQuillContainer />
 
 			<br />
 			<br />
