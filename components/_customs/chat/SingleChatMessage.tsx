44b482937437d3c85261a2745697bece0be033e6
변지욱 <sgo1017099@mobmail.shinhan.com>
2022-08-08 11:27:15 +0900

    doing chat

--- components/_customs/chat/SingleChatMessage.tsx
+++ components/_customs/chat/SingleChatMessage.tsx
@@ -1,19 +1,44 @@
 import { useState } from 'react';
 import { Avatar, Button } from '@components/index';
 import { techImage } from '@utils/constants/techs';
-import { Divider, Label } from 'semantic-ui-react';
+import { Label } from 'semantic-ui-react';
+import classNames from 'classnames/bind';
+import { useModal } from '@utils/hooks/customHooks';
+import { modalUISize } from '@utils/constants/uiConstants';
 import Image from 'next/image';
+import { ChatList } from '@utils/types/commTypes';
 import Style from './SingleChatMessage.module.scss';
 
-const SingleChatMessage = ({
-	messageOwner,
-	context,
-}: {
-	messageOwner: string;
-	context: string;
-}) => {
+interface ChatListExtends extends ChatList {
+	messageOwner: 'other' | 'mine';
+}
+
+const SingleChatMessage = ({ messageOwner, value, imgList, linkList }: ChatListExtends) => {
 	const [showCopyButton, setShowCopyButton] = useState(false);
 	const [copyButtonClicked, setCopyButtonClicked] = useState(false);
+
+	const { handleModal } = useModal();
+
+	const openImageModal = (imgSrc: string) => {
+		handleModal({
+			modalOpen: true,
+			modalContent: (
+				<img
+					src={imgSrc}
+					style={{
+						maxHeight: '90%',
+						maxWidth: '90%',
+						display: 'block',
+						margin: 'auto',
+					}}
+				/>
+			),
+			modalSize: modalUISize.LARGE,
+			modalIsBasic: true,
+		});
+	};
+
+	const cx = classNames.bind(Style);
 
 	return (
 		<>
@@ -25,14 +50,7 @@ const SingleChatMessage = ({
 					setCopyButtonClicked(false);
 				}}
 			>
-				<div
-					style={{
-						alignSelf: `${messageOwner === 'other' ? 'self-start' : 'self-end'}`,
-						display: 'flex',
-						flexDirection: 'column',
-					}}
-					className={Style['singleChatDiv']}
-				>
+				<div className={cx('singleChatDiv', messageOwner)}>
 					<Label
 						attached={`top ${messageOwner === 'other' ? 'left' : 'right'}`}
 						className={Style['avatarLabel']}
@@ -48,35 +66,61 @@ const SingleChatMessage = ({
 					<Label
 						basic
 						pointing={`${messageOwner === 'other' ? 'left' : 'right'}`}
-						style={{
-							maxWidth: '100%',
-							borderColor: `${
-								messageOwner === 'other' ? 'darkorange' : 'darkmagenta'
-							}`,
-							color: 'black !important',
-							alignSelf: `${messageOwner === 'other' ? 'self-start' : 'self-end'}`,
-						}}
+						className={cx('messageLabel', messageOwner)}
 					>
-						<pre className={Style['preClass']}>{`${context.replaceAll(
-							'\t',
-							' '.repeat(3),
-						)}`}</pre>
+						<pre>{`${value.replaceAll('\t', ' '.repeat(3))}`}</pre>
 					</Label>
-					<div
-						style={{
-							marginTop: '10px',
-							display: 'flex',
-							gap: '8px',
-							alignSelf: `${messageOwner === 'other' ? 'self-start' : 'self-end'}`,
-						}}
-					>
-						<Image src={techImage['React']} height={50} width={50} />
-						<Image src={techImage['React']} height={50} width={50} />
-						<Image src={techImage['React']} height={50} width={50} />
-						<Image src={techImage['React']} height={50} width={50} />
-						<Image src={techImage['React']} height={50} width={50} />
-						<Image src={techImage['React']} height={50} width={50} />
-					</div>
+					{imgList?.length > 0 && (
+						<div className={cx('imageListDiv', messageOwner)}>
+							{imgList.map((item: { fileName: string; filePreview: string }, idx) => (
+								<Image
+									key={`asdf_${idx}`}
+									src={item.filePreview}
+									height={50}
+									width={50}
+									onClick={() => openImageModal(item.filePreview)}
+								/>
+							))}
+						</div>
+					)}
+					{/* <div className={cx('imageListDiv', messageOwner)}>
+						<Image
+							src={techImage['React']}
+							height={50}
+							width={50}
+							onClick={() => openImageModal(techImage['React'])}
+						/>
+						<Image
+							src={techImage['Docker']}
+							height={50}
+							width={50}
+							onClick={() => openImageModal(techImage['Docker'])}
+						/>
+						<Image
+							src={techImage['Node']}
+							height={50}
+							width={50}
+							onClick={() => openImageModal(techImage['Node'])}
+						/>
+						<Image
+							src={techImage['Typescript']}
+							height={50}
+							width={50}
+							onClick={() => openImageModal(techImage['Typescript'])}
+						/>
+						<Image
+							src={techImage['Express']}
+							height={50}
+							width={50}
+							onClick={() => openImageModal(techImage['Express'])}
+						/>
+						<Image
+							src={techImage['Vue']}
+							height={50}
+							width={50}
+							onClick={() => openImageModal(techImage['Vue'])}
+						/>
+					</div> */}
 				</div>
 				{showCopyButton && (
 					<div className={Style['copyButton']}>
@@ -87,9 +131,9 @@ const SingleChatMessage = ({
 							onClick={async () => {
 								setCopyButtonClicked(true);
 								if ('clipboard' in navigator) {
-									await navigator.clipboard.writeText(context);
+									await navigator.clipboard.writeText(value);
 								} else {
-									return document.execCommand('copy', true, context);
+									return document.execCommand('copy', true, value);
 								}
 								setTimeout(() => {
 									setCopyButtonClicked(false);
