357358f7412dd2ecd75d2f2abe5e6730b375b6f8
변지욱 <sgo1017099@mobmail.shinhan.com>
2022-09-21 15:01:25 +0900

    fixing bug not yet completed

--- pages/chat/[userId].tsx
+++ pages/chat/[userId].tsx
@@ -7,23 +7,25 @@
  * 2      변지욱     2022-08-29   feature/JW/chat        유저명 표시하도록 변경, socket에서 직접 채팅리스트 가져오도록 변경
  * 3      변지욱     2022-08-29   feature/JW/layoutchat  최초 로드 시엔 변경중입니다 텍스트 안 보이게 변경
  * 4      변지욱     2022-09-06   feature/JW/chatPage    누구랑 채팅하는지 세팅
+ * 5      변지욱     2022-09-21
  ********************************************************************************************/
 
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState, useMemo } from 'react';
 import { Avatar, Box, DTechQuill, SharpDivider, TextWithDotAnimation } from '@components/index';
 import { MainLayoutTemplate, SingleChatMessage } from '@components/customs';
 import { Container, Segment } from 'semantic-ui-react';
 import dynamic from 'next/dynamic';
 
-import { ChatList, IUsersStatusArr, IAuth } from '@utils/types/commAndStoreTypes';
+import { ChatList, IUsersStatusArr, IAuth, IAppCommon } from '@utils/types/commAndStoreTypes';
 import OnlineSvg from '@styles/svg/online.svg';
 import OfflineSvg from '@styles/svg/offline.svg';
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
 import { useSelector, useDispatch } from 'react-redux';
 import dayjs from 'dayjs';
 import { toast } from 'react-toastify';
-import lodash from 'lodash';
 import cookie from 'js-cookie';
+import lodash from 'lodash';
+import { parseCookies } from 'nookies';
 
 import Style from './[userId].module.scss';
 
@@ -56,27 +58,37 @@ const dayOfWeek: { [val: string]: string } = {
 	'6': '일요일',
 };
 
-const ReactQuill = dynamic(
-	async () => {
-		const { default: RQ } = await import('react-quill');
+const chatToDateGroup = (arr: any) => {
+	const groupsReduce = arr.reduce((previouseVal: any, currentVal: any) => {
+		const date = currentVal.SENT_DATETIME.split('T')[0];
 
-		return function comp({ forwardedRef, ...props }: any) {
-			return <RQ ref={forwardedRef} {...props} />;
-		};
-	},
-	{ ssr: false },
-);
+		const hourMin = dayjs(currentVal.SENT_DATETIME).format('HH:mm');
+
+		if (!previouseVal[date]) {
+			previouseVal[date] = {};
+		}
+		if (!previouseVal[date][hourMin]) {
+			previouseVal[date][hourMin] = [];
+		}
+		previouseVal[date][hourMin].push(currentVal);
+		return previouseVal;
+	}, {});
+
+	return groupsReduce;
+};
 
 const UserChat = ({
 	usersStatusArr,
 	queryObj,
+	chatListSSR,
 }: {
 	usersStatusArr: IUsersStatusArr[];
 	queryObj: any;
+	chatListSSR: any;
 }) => {
 	const [quillWrapperHeight, setQuillWrapperHeight] = useState(0);
 	const [chatUser, setChatUser] = useState<{ [name: string]: string }>();
-	const [chatList, setChatList] = useState<ChatDateReduce>({});
+	const [chatList, setChatList] = useState<ChatDateReduce>(chatListSSR);
 	const [textChangeNotification, setTextChangeNotification] = useState(false);
 	const [sendingUserState, setSendingUserState] = useState<string>('');
 	const conversationId = useRef<string>();
@@ -85,43 +97,30 @@ const UserChat = ({
 	const firstLoadRef = useRef<boolean>(true);
 	const quillRef = useRef<any>(null);
 
-	const { userId: userUID } = queryObj; // UID in here
+	const userUID = useMemo(() => {
+		return queryObj.userId;
+	}, [queryObj.userId]);
+
+	// const { userId: userUID } = queryObj; // UID in here
+
+	const authStore = useSelector((state: { auth: IAuth }) => state.auth);
+	const appCommon = useSelector((state: { appCommon: IAppCommon }) => state.appCommon);
+	const socket = authStore.userSocket;
 
 	const dispatch = useDispatch();
 
 	useEffect(() => {
 		dispatch({ type: 'SET_CURRENT_CHAT_USER', chatUser: userUID });
-		cookie.set('currentChatUser', userUID);
 
 		return () => {
 			dispatch({ type: 'SET_CURRENT_CHAT_USER', chatUser: '' });
 		};
 	}, [dispatch, userUID]);
 
-	const authStore = useSelector((state: { auth: IAuth }) => state.auth);
-	const socket = authStore.userSocket;
-
-	const chatToDateGroup = (arr: any) => {
-		const groupsReduce = arr.reduce((previouseVal: any, currentVal: any) => {
-			const date = currentVal.SENT_DATETIME.split('T')[0];
-
-			const hourMin = dayjs(currentVal.SENT_DATETIME).format('HH:mm');
-
-			if (!previouseVal[date]) {
-				previouseVal[date] = {};
-			}
-			if (!previouseVal[date][hourMin]) {
-				previouseVal[date][hourMin] = [];
-			}
-			previouseVal[date][hourMin].push(currentVal);
-			return previouseVal;
-		}, {});
-
-		return groupsReduce;
-	};
-
 	useEffect(() => {
-		userUID &&
+		const { currentChatUser } = appCommon;
+
+		if (currentChatUser) {
 			axios
 				.get('http://localhost:3066/api/auth/getUsersInfo', {
 					params: { usersParam: [userUID] },
@@ -132,119 +131,12 @@ const UserChat = ({
 				.catch(() => {
 					toast['error'](<>{'유저정보를 가져오지 못했습니다'}</>);
 				});
-	}, [userUID]);
-
-	const getPrivateChatListFunction = useCallback(
-		(successCallback: Function, errorCallback: Function) => {
-			if (authStore.userUID && authStore.userToken && userUID) {
-				axios
-					.post(
-						'http://localhost:3066/api/chat/getPrivateChatList',
-						{ fromUID: authStore.userUID, toUID: userUID },
-						{
-							headers: { Authorization: `Bearer ${authStore.userToken}` },
-						},
-					)
-					.then((response) => successCallback(response))
-					.catch((err) => errorCallback(err));
-			}
-		},
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-		[authStore.userToken, authStore.userUID, userUID],
-	);
-
-	const getPrivateChatListAxios = useCallback(
-		(chatListSocket: any = null, convIdSocket: string = '') => {
-			if (
-				lodash.isEmpty(chatListSocket) ||
-				(lodash.isArray(chatListSocket) && chatListSocket.length === 0)
-			) {
-				getPrivateChatListFunction(
-					(response: AxiosResponse<any, any>) => {
-						conversationId.current = response.data.convId;
-						const groupsReduce = chatToDateGroup(response.data.chatList);
-
-						setChatList(groupsReduce);
-					},
-					(err: any) => {
-						toast['error'](<>{'채팅정보를 가져오지 못했습니다'}</>);
-					},
-				);
-			} else {
-				conversationId.current = convIdSocket;
-				const groupsReduce = chatToDateGroup(chatListSocket);
-
-				setChatList(groupsReduce);
-			}
-		},
-		[getPrivateChatListFunction],
-	);
-
-	useEffect(() => {
-		getPrivateChatListAxios();
-	}, [getPrivateChatListAxios]);
+		}
+	}, [appCommon, userUID]);
 
 	useEffect(() => {
 		bottomRef.current?.scrollIntoView({ behavior: 'auto' });
 	}, [chatList, quillWrapperHeight]);
-
-	const sendMessageFunction = useCallback(
-		async (content: ChatList) => {
-			const formData = new FormData();
-			const postData = {
-				dir: 'chat/',
-			};
-
-			formData.append('postData', JSON.stringify(postData));
-
-			for (let i = 0; i < content.imgList.length; i++) {
-				formData.append(
-					'img',
-					content.imgList[i].imageFile,
-					`${content.imgList[i].fileName}`,
-				);
-			}
-
-			await axios
-				.post('http://localhost:3066/api/chat/uploadChatImg', formData)
-				.then((response) => {
-					socket?.emit('sendPrivateMessage', {
-						chatMessage: content.value,
-						userUID: authStore.userUID,
-						convId: conversationId.current,
-						imgList: JSON.stringify(
-							content.imgList.length !== 0
-								? response.data.bodyObj.imgArr.map(
-										(urlString: string) =>
-											`${process.env.NEXT_PUBLIC_IMG_S3}${urlString}`,
-								  )
-								: [],
-						),
-						linkList: JSON.stringify(content.linkList),
-						toUserId: chatUser && chatUser.USER_ID,
-					});
-				})
-				.catch(() => {
-					console.log('image S3 error');
-				});
-
-			socket?.on('messageSendSuccess', ({ chatListSocket, convIdSocket }: any) => {
-				getPrivateChatListAxios(chatListSocket, convIdSocket);
-			});
-		},
-		[authStore.userUID, chatUser, getPrivateChatListAxios, socket],
-	);
-
-	useEffect(() => {
-		socket?.on('newMessageReceived', ({ chatListSocket, convIdSocket, fromUID }: any) => {
-			getPrivateChatListAxios(chatListSocket, convIdSocket);
-		});
-		socket?.on('textChangeNotification', (sendingUser: string) => {
-			setSendingUserState(sendingUser);
-			setTextChangeNotification(true);
-		});
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [socket]);
 
 	const notifyTextChange = useCallback(() => {
 		if (!firstLoadRef.current) {
@@ -263,6 +155,80 @@ const UserChat = ({
 			if (textChangeNotification) setTextChangeNotification(false);
 		}, 3500);
 	}, [textChangeNotification]);
+
+	const sendPrivateMessageSocket = (content: ChatList, imgArr = []) => {
+		socket?.emit('sendPrivateMessage', {
+			chatMessage: content.value,
+			userUID: authStore.userUID,
+			convId: conversationId.current,
+			imgList: JSON.stringify(
+				imgArr.length !== 0
+					? imgArr.map(
+							(urlString: string) => `${process.env.NEXT_PUBLIC_IMG_S3}${urlString}`,
+					  )
+					: [],
+			),
+			linkList: JSON.stringify(content.linkList),
+			toUserId: chatUser && chatUser.USER_ID,
+			toUserUID: appCommon.currentChatUser,
+		});
+	};
+
+	const sendMessageFunction = async (content: ChatList) => {
+		if (content.imgList.length > 0) {
+			const formData = new FormData();
+			const postData = {
+				dir: 'chat/',
+			};
+
+			formData.append('postData', JSON.stringify(postData));
+
+			for (let i = 0; i < content.imgList.length; i++) {
+				formData.append(
+					'img',
+					content.imgList[i].imageFile,
+					`${content.imgList[i].fileName}`,
+				);
+			}
+
+			await axios
+				.post('http://localhost:3066/api/chat/uploadChatImg', formData)
+				.then((response) => {
+					sendPrivateMessageSocket(content, response.data.bodyObj.imgArr);
+				})
+				.catch(() => {
+					toast['error'](<>{'이미지를 보내지 못했습니다'}</>);
+				});
+		} else {
+			sendPrivateMessageSocket(content);
+		}
+	};
+
+	useEffect(() => {
+		socket?.on('messageSendSuccess', ({ chatListSocket, convIdSocket, toUserUID }: any) => {
+			if (appCommon.currentChatUser === toUserUID) {
+				// alert(
+				// 	`messageSendSuccess and currentChatUser is ${appCommon.currentChatUser} and toUserUID is ${toUserUID}`,
+				// );
+
+				const cloneObjReduce = chatToDateGroup(lodash.cloneDeep(chatListSocket));
+
+				// setChatList((prev) => cloneObjReduce);
+			}
+		});
+
+		socket?.on('newMessageReceived', ({ chatListSocket, convIdSocket, fromUID }: any) => {
+			if (userUID === fromUID) {
+				console.log(`current is ${appCommon.currentChatUser} and fromUID is ${fromUID}`);
+				// getPrivateChatListCallback();
+			}
+		});
+
+		// socket?.on('textChangeNotification', (sendingUser: string) => {
+		// 	setSendingUserState(sendingUser);
+		// 	setTextChangeNotification(true);
+		// });
+	}, [socket, appCommon.currentChatUser, userUID]);
 
 	return (
 		<>
@@ -376,7 +342,7 @@ const UserChat = ({
 								console.log(content);
 								sendMessageFunction(content);
 							}}
-							QuillSSR={ReactQuill}
+							// QuillSSR={ReactQuill}
 							notifyTextChange={notifyTextChange}
 						/>
 						<TextWithDotAnimation
@@ -401,7 +367,23 @@ UserChat.displayName = 'chatPage';
 // });
 
 export const getServerSideProps = async (context: any) => {
-	return { props: { queryObj: context.query } };
+	const { token, currentChatUser } = parseCookies(context);
+
+	const axiosData = await axios
+		.post(
+			'http://localhost:3066/api/chat/getPrivateChatList2',
+			{ toUID: currentChatUser },
+			{
+				headers: { Authorization: `Bearer ${token}` },
+			},
+		)
+		.then((response) => {
+			const chatGroupReduce = chatToDateGroup(response.data.chatList);
+
+			return chatGroupReduce;
+		});
+
+	return { props: { queryObj: context.query, chatListSSR: axiosData } };
 };
 
 export default UserChat;
