4bd94434ed91bed5011852f9b96078c920f85ace
변지욱 <biglol@nate.com>
2022-10-09 14:52:46 +0900

    storybook store modified

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,7 +2,7 @@ import 'semantic-ui-css/semantic.min.css';
 import 'react-quill/dist/quill.snow.css';
 
 import { Provider } from 'react-redux';
-import { makeStore } from '@store/rootReducer';
+import { store } from '@store/rootReducer';
 import { ModalPopup } from '@components/index';
 
 export const parameters = {
@@ -19,7 +19,7 @@ export const parameters = {
 // 20220801... Redux + modal 적용
 export const decorators = [
 	(Story) => (
-		<Provider store={makeStore()}>
+		<Provider store={store}>
 			<Story />
 			<ModalPopup />
 		</Provider>
