3f7c2467d83b7c2276da8aa0164122cd232e2c2b
biglol <biglol@nate.com>
2022-10-25 14:24:21 +0900

    cleared console and unused vars

--- components/quill/DTechQuill.tsx
+++ components/quill/DTechQuill.tsx
@@ -66,31 +66,31 @@ const ReactQuill = dynamic(
 // const contentType = block[0].split(':')[1];
 // const realData = block[1].split(',')[1];
 
-// const blob = b64toBlob(realData, contentType);
-const b64toBlob = (b64Data: any, contentTypeParam: any, sliceSizeParam: any = 0) => {
-	if (b64Data === '' || b64Data === undefined) return null;
+// ! const blob = b64toBlob(realData, contentType);
+// const b64toBlob = (b64Data: any, contentTypeParam: any, sliceSizeParam: any = 0) => {
+// 	if (b64Data === '' || b64Data === undefined) return null;
 
-	const contentType = contentTypeParam || '';
-	const sliceSize = sliceSizeParam || 512;
-	const byteCharacters = atob(b64Data);
-	const byteArrays = [];
+// 	const contentType = contentTypeParam || '';
+// 	const sliceSize = sliceSizeParam || 512;
+// 	const byteCharacters = atob(b64Data);
+// 	const byteArrays = [];
 
-	for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-		const slice = byteCharacters.slice(offset, offset + sliceSize);
-		const byteNumbers = new Array(slice.length);
+// 	for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+// 		const slice = byteCharacters.slice(offset, offset + sliceSize);
+// 		const byteNumbers = new Array(slice.length);
 
-		for (let i = 0; i < slice.length; i++) {
-			byteNumbers[i] = slice.charCodeAt(i);
-		}
-		const byteArray = new Uint8Array(byteNumbers);
+// 		for (let i = 0; i < slice.length; i++) {
+// 			byteNumbers[i] = slice.charCodeAt(i);
+// 		}
+// 		const byteArray = new Uint8Array(byteNumbers);
 
-		byteArrays.push(byteArray);
-	}
+// 		byteArrays.push(byteArray);
+// 	}
 
-	const blob = new Blob(byteArrays, { type: contentType });
+// 	const blob = new Blob(byteArrays, { type: contentType });
 
-	return blob;
-};
+// 	return blob;
+// };
 
 const DTechQuill = forwardRef<any, IDTechQuill>(
 	(
